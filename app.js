ff0bf2e1306fac00eec672b2b45a03514671defc
omprakashnanda323-hash <omprakashnanda323@gmail.com>
2026-06-12 20:45:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUxAiC17gKvnyVAdZEuQ0IEi3ctzckd_Y",
+  authDomain: "dailytask-earning.firebaseapp.com",
+  databaseURL: "https://dailytask-earning-default-rtdb.firebaseio.com",
+  projectId: "dailytask-earning",
+  storageBucket: "dailytask-earning.firebasestorage.app",
+  messagingSenderId: "1054491992628",
+  appId: "1:1054491992628:web:e5f4820e9a77ef38252061"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 document.getElementById("joinBtn").addEventListener("click", () => {
     window.open("https://t.me/+sVoxUBy6_8A3MDVl", "_blank");
+
+    firebase.database().ref("clicks/telegram").push({
+        time: Date.now()
+    });
 });
 
 document.getElementById("youtubeBtn").addEventListener("click", () => {
     window.open("https://www.youtube.com/@omprakashnanda4140", "_blank");
+
+    firebase.database().ref("clicks/youtube").push({
+        time: Date.now()
+    });
 });
 
 document.getElementById("refBtn").addEventListener("click", () => {
